01131b2428071fd2bb98a693ccd2e8673df7e669
DoubtfulTurnip <23531986+DoubtfulTurnip@users.noreply.github.com>
2025-02-06 14:49:51 +1300

    Update CKPT_PTH.py

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,5 @@
-LLAVA_CLIP_PATH = '/opt/data/private/AIGC_pretrain/LLaVA1.5/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = '/opt/data/private/AIGC_pretrain/LLaVA1.5/llava-v1.5-13b'
-SDXL_CLIP1_PATH = '/opt/data/private/AIGC_pretrain/clip-vit-large-patch14'
-SDXL_CLIP2_CKPT_PTH = '/opt/data/private/AIGC_pretrain/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
+
+LLAVA_MODEL_PATH = '/workspace/SUPIR/models/LLaVA v1.5 13B/llava-v1.5-13b'
+LLAVA_CLIP_PATH = '/workspace/SUPIR/models/LLaVA CLIP/clip-vit-large-patch14-336'
+SDXL_CLIP1_PATH = '/workspace/SUPIR/models/SDXL CLIP Encoder-1/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = '/workspace/SUPIR/models/SDXL CLIP Encoder-2/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
